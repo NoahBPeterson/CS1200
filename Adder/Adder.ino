3a9174f1da38da719307cdec310ab823f46ea81e
LoadingPleaseWait <michael@murphey.org>
2016-11-03 10:11:07 -0500

    Rename Arduino files and add LED constants

---
+++ Adder/Adder.ino
@@ -0,0 +1,21 @@
+#include "Addition.h"
+
+const int LED_1 = 2;
+const int LED_2 = 3;
+const int LED_3 = 4;
+const int LED_4 = 5;
+const int LED_5 = 6;
+const int LED_6 = 7;
+const int LED_7 = 8;
+const int LED_8 = 9;
+const int LED_9 = 10;
+const int LED_10 = 11;
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
+
